544234aaff69611f12daef87fcfb6abdd0175081
Nam Nguyễn <nhpn2003@gmail.com>
2024-04-11 21:19:22 +0700

    login with google and facebook

--- Doan_Web_CK/obj/Debug/net8.0/Doan_Web_CK.AssemblyInfo.cs
+++ Doan_Web_CK/obj/Debug/net8.0/Doan_Web_CK.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Doan_Web_CK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2959385d4cab9159397aeae64f311a1ead0d79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b0955080483737cc268123a922698403309753")]
 [assembly: System.Reflection.AssemblyProductAttribute("Doan_Web_CK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Doan_Web_CK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
